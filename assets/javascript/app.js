4d6381d7543b84ec024b25f5188b112e98afb1ca
jdurant0529 <james.durant@gmail.com>
2016-05-19 19:04:26 -0400

    moving stuff around - chat sorta works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,24 +4,25 @@ $(document).ready(function() {
     var Players = new Firebase("https://rps-jrd.firebaseio.com/");
     var Player1 = Players.child("Player1");
     var Player2 = Players.child("Player2");
+    var chat = Players.child("chats");
 
     var user = 0;
+
+    var blankData = {};
 
     var playersExist = false;
 
     var player1Exists = false;
-    var Player1Data = {};
     var player2Exists = false;
+
+    var Player1Data = {};
     var Player2Data = {};
+
     var gameset = false;
 
     //game setup
     function gameSetup() {
 
-        console.log(gameset);
-
-        console.log("here we need to set the game");
-        console.log("you = ")
 
 
         console.log("playersExist = " + playersExist);
@@ -40,23 +41,63 @@ $(document).ready(function() {
                 } else if (player1Exists && player2Exists) {
                     $('#newPlayer').hide();
                 }
+            } else {
+                chats.remove();
             }
             console.log("player1Exists = " + player1Exists);
             console.log("player2Exists = " + player2Exists);
             gameset = true;
         }
 
-        return false;
-
     } //end of gameSetup
+    function newChat() { //start of function to add a new chat message.
+        var chittyChat = "<p><strong>" + you.name + "</strong>: " + $('#chatText').val().trim() + "<p>"
+        chat.push(chittyChat);
+        $('#chatText').val('');
+        //console.log($('.form-control').val().trim());
+        //$('.chat-box').append("<p>" + you.name + ": " + $('#chatText').val().trim() + "<p>");
 
+
+    } //end of function to add a new chat message.
     function newPlayer() {
 
         console.log("inside newPlayer function")
         var playerName = $('#playerName').val().trim();
 
+
         if (playerName != '') {
-            if (!playersExist) {
+            if (playersExist) {
+                if (player2Exists) {
+                    console.log('setting user as player1');
+                    $('#playerNumber').html("<h3>Hi " + playerName + ", you are player 1</h3>");
+                    $('#P1').html(playerName)
+                    $('#playerName').val("");
+                    $('#newPlayer').hide();
+                    you = {
+                        name: playerName,
+                        choice: '',
+                        wins: 0,
+                        losses: 0,
+
+                    }
+                    Player1.set(you)
+                    user = 1;
+                } else if (player1Exists) {
+                    $('#playerNumber').html("<h3>Hi " + playerName + ", you are player 2</h3>");
+                    $('#P2').html(playerName)
+                    $('#playerName').val("");
+                    $('#newPlayer').hide();
+                    you = {
+                        name: playerName,
+                        choice: '',
+                        wins: 0,
+                        losses: 0
+                    }
+                    Player2.set(you)
+                    user = 2;
+                }
+            } else {
+                console.log('setting user as player1');
                 $('#playerNumber').html("<h3>Hi " + playerName + ", you are player 1</h3>");
                 $('#P1').html(playerName)
                 $('#playerName').val("");
@@ -66,45 +107,36 @@ $(document).ready(function() {
                     choice: '',
                     wins: 0,
                     losses: 0,
-               
+
                 }
                 Player1.set(you)
                 user = 1;
 
-            } else if (player1Exists) {
-                $('#playerNumber').html("<h3>Hi " + playerName + ", you are player 2</h3>");
-                $('#P2').html(playerName)
-                $('#playerName').val("");
-                $('#newPlayer').hide();
-                you = {
-                    name: playerName,
-                    choice: '',
-                    wins: 0,
-                    losses: 0
-                }
-                Player2.set(you)
-                user = 2;
             }
 
         }
         return false;
-    }
+    } // end of newPLayer function
 
 
 
-    Players.on("value", function(snapshot) {
+    Players.on("child_added", function(childSnapshot) {
             console.log("-----start of players------")
 
             console.log("Players exists...")
-            console.log(snapshot.exists());
-            if (snapshot.exists()) {
+            console.log(childSnapshot.exists());
+            if (childSnapshot.exists()) {
                 playersExist = true;
             }
             console.log("Players snapshot value")
-            console.log(snapshot.val());
+            console.log(childSnapshot.val());
             console.log("-----end of players-----")
 
-            Player1.on("value", function(snapshot) {
+           
+
+            gameSetup();
+        }) // end of players on value function
+     Player1.on("value", function(snapshot) {
                     console.log("------start of player1------")
                     console.log("Player1 exists...")
                     console.log(snapshot.exists());
@@ -135,8 +167,10 @@ $(document).ready(function() {
 
                 }) //end of player2 value
 
-            gameSetup();
-        }) // end of players on value function
+   chat.on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+        $('.chat-box').append(childSnapshot.val())
+    })
 
     $(window).unload(function() {
         console.log('start of page unload.');
@@ -144,15 +178,15 @@ $(document).ready(function() {
 
         if (user == 1) {
             console.log("you were player 1.  Player 1 has left the game").
-            Player1.remove();
+            Players.child("Player1").remove();
         } else if (user == 2) {
             console.log("you were player 2.  player 2 has left the game.")
-            Player2.remove();
+            Player1.remove();
         }
         console.log('end of page unload');
     });
 
-
-
     $(document).on('click', '#submit', newPlayer);
+
+    $(document).on('click', '#sendChat', newChat);
 })
--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
     var Player1 = Players.child("Player1");
     var Player2 = Players.child("Player2");
 
-    var you = {};  // player 
+    var user = 0;   // to use when player reloads page
+
+     
     
+
+
+
 // }  end of document ready
